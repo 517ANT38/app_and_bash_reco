b5876195d99865176caa769a228b9b796aa45fab
517ANT38 <a.tashchilin@bk.ru>
2023-11-17 06:23:44 +0300

    fix restore bash

--- app_start/restore.bash
+++ app_start/restore.bash
@@ -1,4 +1,6 @@
 #!/bin/bash
 cp -r $1 /app
+echo -n "Пользователь БД $USER ввидите "
 psql -U $USER -W -h "localhost" -p 5432 --file=app_start/create_database.sql
-pg_restore -U $USER -W -h localhost -p 5432 -d appmarks $2
+echo -n "Пользователь БД $USER ввидите "
+psql -U $USER -W -h localhost -p 5432 -d appmarks --file=$2
